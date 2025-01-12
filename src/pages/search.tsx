a807b872d6faef5502bdd192fde5636e68640ac9
pkyujin <luxoris02@sookmyung.ac.kr>
2025-01-12 14:36:37 +0900

    feat: 로그인/회원가입 페이지 상단 로고 컴포넌트 제작

---
+++ src/pages/search.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Search = () => {
+  return null;  // 아무것도 렌더링하지 않음
+};
+
+export default Search;
---
+++ src/pages/signup.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Signup = () => {
+  return null;  // 아무것도 렌더링하지 않음
+};
+
+export default Signup;
