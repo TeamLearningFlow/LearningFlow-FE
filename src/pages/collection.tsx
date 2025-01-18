79b684bab42d54bd277d6fabb25459646ff01c98
pkyujin <luxoris02@sookmyung.ac.kr>
2025-01-19 00:36:33 +0900

    feat: 컬렉션 페이지 제목 컴포넌트 제작 #16

--- src/pages/collection.tsx
+++ src/pages/collection.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/collectionHeader';
+import TitleBar from './components/collectionTitleBar';
 
 const CollectionPage: React.FC = () => {
   return (
     <>
       <Header />
+      <TitleBar />
     </>
   );
 };
