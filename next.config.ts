ccc4fa7bb75215b2d0c6b5f95c51ad76c517c4ff
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-01-20 23:28:03 +0900

    merge: 로그인 페이지 병합 완료

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // experimental: {
+  //   externalDir: true, // 외부 폴더도 Next.js가 인식하게 설정
+  // },
 };
 
 export default nextConfig;
