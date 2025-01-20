0e4c434d16834f9cb73229a070ad1349014abd2a
jooyeong <juyeong@sookmyung.ac.kr>
2025-01-20 21:48:05 +0900

    feat: 검색 페이지 보딩패스 컴포넌트 제작 #17

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/searchHeader';
+import BoardingPass from './components/boardingPass';
 
 const SearchPage: React.FC = () => {
   return (
     <>
       <Header />
+      <BoardingPass />
     </>
   );
 };
