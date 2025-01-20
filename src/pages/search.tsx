4ecfc52c12cea59b39d7275d9898fb64a73dc20e
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-01-20 22:21:30 +0900

    feat: 검색 페이지 카테고리 리스트 상단 제작 #18

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/searchHeader';
+import CategoryList from './components/categoryList';
 
 const SearchPage: React.FC = () => {
   return (
     <>
       <Header />
+      <CategoryList />
     </>
   );
 };
