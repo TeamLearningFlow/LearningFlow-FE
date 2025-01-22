26392085d854137d9f26f7865a48d2733a70610f
장자윤 <jayoonchang@naver.com>
2025-01-22 20:25:23 +0900

    feat: 회원가입 페이지 이메일 인증 모달 제작 #21

--- src/pages/emailAuth.tsx
+++ src/pages/emailAuth.tsx
@@ -1,69 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import TopLogo from './components/toplogo_dark';
+import TopLogo from './components/toplogo_guest';
 import EmailIcon from './assets/mailIcon.png';
 import Image from 'next/image';
+import EmailAuthModal from './modal/emailAuthModal';
+
+const EmailAuthPage: React.FC = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const handleModalOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <Container>
+      <TopLogo />
+      <PageContainer>
+        <TitleContainer>인증 메일을 보내드렸어요</TitleContainer>
+        <IconContainer>
+          <Image src={EmailIcon} alt="Email Icon" width={240} height={240} />
+        </IconContainer>
+        <Textwrapper>
+          {`메일함을 확인해주세요\n가입하신 이메일을 인증해 주시면,\n온보딩의 서비스를 마음껏 이용하실 수 있어요`}
+        </Textwrapper>
+        <GoBackButton onClick={handleModalOpen}>
+          이전 페이지로 돌아가기
+        </GoBackButton>
+        {showModal && <EmailAuthModal onClose={handleModalClose} />}
+      </PageContainer>
+    </Container>
+  );
+};
+
+export default EmailAuthPage;
+
+const Container = styled.div`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 18vh;
+
+  margin-top: 145px;
 `;
 
 const TitleContainer = styled.div`
   text-align: center;
-  font-size: 24px;
+  font-size: 30px;
   font-weight: 600;
 `;
 
 const IconContainer = styled.div`
   display: flex;
-  margin: 35px;
+  margin: 32px;
 `;
 
 const Textwrapper = styled.div`
   white-space: pre-line;
   text-align: center;
-  font-size: 15px;
-  margin-bottom: 35px;
+  font-size: 20px;
+  margin-bottom: 32px;
+  line-height: 28px;
 `;
 
 const GoBackButton = styled.button`
   display: flex;
-  height: 45px;
-  padding: 18px 8px;
+  width: 532px;
+  padding: 18px 0px;
+
   justify-content: center;
   align-items: center;
-  width: 27vw;
-  background-color:rgba(94, 82, 255, 1);
+  background-color: rgba(94, 82, 255, 1);
   border: none;
-  border-radius: 5px;
-  font-family: Roboto;
-  font-size: 15px;
-  font-style: normal;
+  border-radius: 16px;
+  font-size: 20px;
+  font-weight: 300;
   color: rgba(255, 255, 255, 1);
   cursor: pointer;
-`
-
-
-const EmailAuthPage: React.FC = () => {
-  return (
-    <>
-      <TopLogo />
-      <PageContainer>
-        <TitleContainer>인증 메일을 보내드렸어요.</TitleContainer>
-        <IconContainer>
-          <Image src={EmailIcon} alt="Email Icon" width={250} height={150} />
-        </IconContainer>
-        <Textwrapper>
-          {`메일함을 확인해주세요.\n가입하신 이메일을 인증해주시면,\n온보딩의 서비스를 마음껏 이용하실 수 있어요.`}
-        </Textwrapper>
-        <GoBackButton>이전 페이지로 돌아가기</GoBackButton>
-      </PageContainer>
-    </>
-  );
-};
-
-export default EmailAuthPage;
+`;
