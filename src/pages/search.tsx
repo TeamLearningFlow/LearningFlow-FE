240569cb49a2543dcb16ea840fd24b71c37253fe
jooyeong <juyeong@sookmyung.ac.kr>
2025-01-23 20:16:24 +0900

    feat: 검색 페이지 페이지 버튼 제작 #24

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Header from './components/searchHeader';
-import CategoryList from './components/categoryList';
-import BoardingPass from './components/boardingPass';
-import Filters from './components/filters';
+import Header from '../components/searchHeader';
+import CategoryList from '../components/categoryList';
+import BoardingPass from '../components/boardingPass';
+import Filters from '../components/filters';
+import Pagination from '@/components/pagination';
 
 const SearchPage: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const SearchPage: React.FC = () => {
       <CategoryList />
       <Filters />
       <BoardingPass />
+      <Pagination />
     </>
   );
 };
