254877b2747e21d74a0bc7b32e324ce58e309118
pkyujin <luxoris02@sookmyung.ac.kr>
2025-01-27 17:18:34 +0900

    feat: 수강 페이지 제목 및 강의 목록 컴포넌트 제작 #30

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from './components/learnHeader';
 import TitleBar from './components/learnTitleBar';
+import ClassTitle from './components/learnClassTitle';
+import ClassList from './components/learnClassList';
 
 const PageWrapper = styled.div``;
 
@@ -10,6 +12,8 @@ const LearnPage: React.FC = () => {
     <PageWrapper>
       <Header />
       <TitleBar />
+      <ClassTitle />
+      <ClassList />
     </PageWrapper>
   );
 };
