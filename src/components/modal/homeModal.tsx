ecb97c74caeae8510f158c6ad0f9f6da833a1f89
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-02-07 04:55:57 +0900

    feat: 홈페이지 모달 컨페티 효과 적용 #55

--- src/components/modal/homeModal.tsx
+++ src/components/modal/homeModal.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-// import Image from 'next/image';
-// import Confetti from '../../assets/confetti.svg';
+import Image from 'next/image';
+
+import { motion } from 'framer-motion';
+import Confetti1 from '../../assets/confetti/confetti1.svg';
+import Confetti2 from '../../assets/confetti/confetti2.svg';
+import Confetti3 from '../../assets/confetti/confetti3.svg';
+import Confetti4 from '../../assets/confetti/confetti4.svg';
+import Confetti5 from '../../assets/confetti/confetti5.svg';
+import Confetti6 from '../../assets/confetti/confetti6.svg';
+import Confetti7 from '../../assets/confetti/confetti7.svg';
+import Confetti8 from '../../assets/confetti/confetti8.svg';
+import Confetti9 from '../../assets/confetti/confetti9.svg';
+// import Confetti10 from '../../assets/confetti/confetti10.svg';
+import Confetti11 from '../../assets/confetti/confetti11.svg';
+import Confetti12 from '../../assets/confetti/confetti12.svg';
+import Confetti13 from '../../assets/confetti/confetti13.svg';
+import Confetti14 from '../../assets/confetti/confetti14.svg';
+import Confetti15 from '../../assets/confetti/confetti15.svg';
+import Confetti16 from '../../assets/confetti/confetti16.svg';
+import Confetti17 from '../../assets/confetti/confetti17.svg';
+import Confetti18 from '../../assets/confetti/confetti18.svg';
+import Confetti19 from '../../assets/confetti/confetti19.svg';
 
 const ModalOverlay = styled.div`
   position: fixed;
@@ -57,13 +77,72 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
+const ConfettiContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  z-index: 1001;
+`;
+
+const Confetti = styled(motion.div)`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+`;
+
 interface ModalProps {
   onClose: () => void;
 }
 
 const HomeModal: React.FC<ModalProps> = ({ onClose }) => {
+  const confettiArray = [
+    Confetti1,
+    Confetti2,
+    Confetti3,
+    Confetti4,
+    Confetti5,
+    Confetti6,
+    Confetti7,
+    Confetti8,
+    Confetti9,
+    // Confetti10,
+    Confetti11,
+    Confetti12,
+    Confetti13,
+    Confetti14,
+    Confetti15,
+    Confetti16,
+    Confetti17,
+    Confetti18,
+    Confetti19,
+  ];
+
   return (
     <>
+      <ConfettiContainer>
+        {confettiArray.map((confetti, i) => (
+          <Confetti
+            key={i}
+            initial={{ top: '-140%', opacity: 0 }}
+            animate={{ top: '0%', opacity: [1, 0] }}
+            transition={{
+              duration: 0.6,
+              delay: i * 0.2,
+              ease: 'easeOut',
+            }}
+            style={{
+              position: 'absolute',
+              left: `${-20 + i * 5}%`,
+            }}
+          >
+            <Image src={confetti} alt={`confetti-${i}`} />
+          </Confetti>
+        ))}
+      </ConfettiContainer>
+
       <ModalOverlay onClick={onClose} />
       <ModalContainer>
         <Title>
@@ -79,7 +158,7 @@ const HomeModal: React.FC<ModalProps> = ({ onClose }) => {
             marginBottom: '15px',
           }}
         >
-          {/* 이미지나 기타 콘텐츠 자리 */}
+          {/* 이미지 */}
         </div>
         <Subtitle>
           필터링을 통해 꼭 맞는 컬렉션으로 <br />
