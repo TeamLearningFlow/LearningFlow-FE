ef4ec4f5d0e2d4e3a48a7cba607aca732539581c
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-02-09 06:46:11 +0900

    style: 검색 페이지 배너 디자인 수정 적용 #59

--- src/components/search/searchBanner.tsx
+++ src/components/search/searchBanner.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
-import BannerText from '../../assets/bannerText.svg';
-import PrevArrowImage from '../../assets//left_arrow_ic.svg';
-import NextArrowImage from '../../assets/right_arrow_ic.svg';
-import LogoMark from '../../assets/logoMark.svg';
+import BannerText from '../../assets/bannerTextP.svg';
+import Logo from '../../assets/bannerLogo.svg';
+import Plane from '../../assets/paperplane.svg';
 
 const BannerWrapper = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 250px;
   padding: 0 10%;
-  background: linear-gradient(90deg,rgba(250, 250, 252, 1),rgba(244, 244, 255, 1));
+  background: linear-gradient(
+    90deg,
+    rgba(250, 250, 252, 1),
+    rgba(244, 244, 255, 1)
+  );
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   overflow: hidden;
   position: relative;
 
@@ -23,55 +26,51 @@ const BannerWrapper = styled.div`
 `;
 
 const LogoWrapper = styled.div`
-  position: absolute;
-  top: 14%;
-  right: 16%;
-  transform: translateY(-50%);
-  width: 120px;
-  height: 120px;
-  pointer-events: none;
-
-  @media (max-width: 850px) {
-    top: 4%;
-    right: 28%;
-    width: 60px;
-    height: 60px;
-  }
-
-  @media (max-width: 560px) {
-    width: 60px;
-    height: 60px;
-  }
-`;
-
-
-const Slide = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 100%;
-  max-width: 100%; 
-  height: 100%;
-  margin-left: 10%;
+  align-items: center;
 
-  @media (max-width: 480px) {
-    margin-left: 0;
-    padding: 0 5%;
+  img {
+    width: 592.895px;
+    height: 661.127px;
   }
 `;
 
 const TextWrapper = styled.div`
-  color: rgba(31, 31, 31, 1);
-  font-size: 30px;
-  font-weight: 600;
-  line-height: 1.5em;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+  max-width: 100%;
+  height: 100%;
+  margin-left: 10%;
+  z-index: 1;
 
-  @media (max-width: 850px) {
-    font-size: 23px;
+  @media (max-width: 768px) {
+    margin-left: 7%;
   }
 
-  @media (max-width: 560px) {
+  @media (max-width: 480px) {
+    margin-left: 5%;
+  }
+`;
+
+const Title = styled.div`
+  color: rgba(31, 31, 31, 1);
+  width: 462px;
+  font-size: 33px;
+  font-weight: 600;
+  line-height: 140%;
+  letter-spacing: -0.76px;
+  justify-content: center;
+
+  @media (max-width: 1024px) {
+    font-size: 30px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 27px;
+  }
+
+  @media (max-width: 480px) {
     font-size: 4vw;
   }
 `;
@@ -80,35 +79,67 @@ const ImageWrapper = styled.div`
   position: relative;
   width: 200px;
   height: 40px;
-  margin-bottom: 0.4vh;
+  margin-bottom: 0.2vh;
 
-  @media (max-width: 850px) {
+  @media (max-width: 768px) {
     width: 180px;
     height: 30px;
     margin-bottom: 0.2vh;
   }
 
-  @media (max-width: 560px) {
+  @media (max-width: 480px) {
     width: 130px;
     height: 25px;
   }
 `;
 
-const Banner: React.FC = () => {
+const PlaneIcon = styled(Image)`
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  top: 25%;
+  left: 53%;
+  transform: rotate(1deg);
 
+  @media (max-width: 1024px) {
+    width: 170px;
+    height: 170px;
+    left: 57%;
+  }
+
+  @media (max-width: 768px) {
+    width: 150px;
+    height: 150px;
+    left: 60%;
+  }
+
+  @media (max-width: 480px) {
+    width: 100px;
+    height: 100px;
+    left: 64%;
+  }
+`;
+
+const Banner: React.FC = () => {
   return (
     <BannerWrapper>
+      <TextWrapper>
+        <ImageWrapper>
+          <Image
+            src={BannerText}
+            alt="Banner Text"
+            fill
+            style={{ objectFit: 'contain' }}
+          />
+        </ImageWrapper>
+        <Title>어디로 학습 여정을 떠나볼까요?</Title>
+      </TextWrapper>
+
+        <PlaneIcon src={Plane} alt="paperplane" />
+    
       <LogoWrapper>
-        <Image src={LogoMark} alt="Logo Mark" width={310} height={310} />
+        <Image src={Logo} alt="Logo Mark" width={592.895} height={661.127} />
       </LogoWrapper>
-          <Slide>
-            <ImageWrapper>
-              <Image src={BannerText} alt="Banner Text" fill style={{ objectFit: 'contain' }} />
-            </ImageWrapper>
-            <TextWrapper>
-              성장을 원하는 사람들을 위한<br />가장 빠른 학습 여정 플랫폼, 온보딩
-            </TextWrapper>
-          </Slide>
     </BannerWrapper>
   );
 };
