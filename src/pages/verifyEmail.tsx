d04b50f619db230f3627b0d3b7f28d8ed7f81406
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-02-16 01:15:37 +0900

    feat: 이메일 변경 api 연결 #65

---
+++ src/pages/verifyEmail.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
+const VerifyEmail: React.FC = () => {
+  const router = useRouter();
+  const { emailResetCode } = router.query; // URL에서 token 추출
+
+  useEffect(() => {
+    const validateToken = async () => {
+      if (!emailResetCode) return; // 토큰이 없는 경우 작업 중단
+
+      try {
+        const response = await axios.get(
+          `http://onboarding.p-e.kr:8080/user/change-email?emailResetCode=${emailResetCode}`,
+        );
+
+        // 유효성 확인 후 페이지 이동
+        if (response.data.isSuccess) {
+          console.log('토큰 유효');
+
+          // 로컬 스토리지에 토큰 저장
+          localStorage.setItem('emailResetCode', emailResetCode as string);
+          console.log('토큰 저장 완료');
+
+          router.push('/mypage/profile'); // 개인 정보 페이지로 이동
+        } else {
+          console.log('토큰 무효');
+          router.push('/home'); // 홈 페이지로 이동
+        }
+      } catch (err) {
+        console.error('토큰 검증 오류:', err);
+        router.push('/home'); // 오류 발생 시 홈 페이지로 이동
+      }
+    };
+
+    validateToken();
+  }, [emailResetCode, router]);
+
+  return null;
+};
+
+export default VerifyEmail;
