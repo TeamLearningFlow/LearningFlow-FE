a488ca79ee5ec0a2e79ec37a7c2f6c3d53083cb0
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-02-17 21:13:59 +0900

    fix: 빌드 오류 수정

--- src/components/mypage/notCompleted.tsx
+++ src/components/mypage/notCompleted.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
-import Prev from '../../assets/Previous.svg';
-import Next from '../../assets/Next.svg';
+import Prev from '../../assets/previous.svg';
+import Next from '../../assets/next.svg';
 import EmptyLearned from '../../components/mypage/emptyLearned';
 
 const NoOverflowDiv = styled.div`
