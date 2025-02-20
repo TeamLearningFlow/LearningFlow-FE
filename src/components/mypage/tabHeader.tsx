aea85bbfa5e5c1a3fb00f4e8475cfa65b8366863
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-02-20 21:27:29 +0900

    feat: 마이페이지 컬렉션 라우팅 설정 #69

--- src/components/mypage/tabHeader.tsx
+++ src/components/mypage/tabHeader.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
-import NotCompleted from './notCompleted';
-import Completed from '../mypage/completed';
-import Liked from './liked';
+// import NotCompleted from './notCompleted';
+// import Completed from '../mypage/completed';
+// import Liked from './liked';
 import { CompletedCollectionData } from '@/types/types';
 
 const Container = styled.div`
@@ -43,8 +43,8 @@ const Menu = styled.span`
   gap: 4px;
 `;
 
-const MenuName = styled.span<{ selected: boolean }>`
-  color: ${(props) => (props.selected ? '#5e52ff' : '#DDE0E4')};
+const MenuName = styled.span<{ learned: boolean }>`
+  color: ${(props) => (props.learned ? '#5e52ff' : '#DDE0E4')};
   text-align: center;
   transition: 0.8s;
 
@@ -62,30 +62,29 @@ const MenuName = styled.span<{ selected: boolean }>`
   }
 `;
 
-const Line = styled.span<{ width: string; selected: boolean }>`
+const Line = styled.span<{ width: string; learned: boolean }>`
   width: ${(props) => props.width};
   height: 2px;
   background: #5e52ff;
-  display: ${(props) => (props.selected ? 'block' : 'none')};
+  display: ${(props) => (props.learned ? 'block' : 'none')};
   transition: 0.8s;
 `;
 
-const TabContents = styled.div`
+/* const TabContents = styled.div`
   width: 1200px;
   margin: 54px 120px 120px 120px;
 
-  /* 반응형 설정 */
   @media (max-width: 1024px) {
     width: 1000px;
     margin: 20px 10px 100px 10px;
   }
 
   @media (max-width: 768px) {
-    width: calc(100vw - 20px); /* 모바일 화면 */
+    width: calc(100vw - 20px);
   }
 
   @media (max-width: 480px) {
-    width: calc(100vw - 10px); /* 폰 화면 */
+    width: calc(100vw - 10px);
     margin: 10px 5px 0 5px;
   }
 `;
@@ -98,23 +97,21 @@ const TabContent = styled.div<{ shown: boolean }>`
   display: ${(props) => (props.shown ? 'flex' : 'none')};
   gap: 54px;
 
-  /* 반응형 설정 */
   @media (max-width: 1024px) {
     width: 1000px;
     margin: 20px 10px 0 10px;
   }
 
   @media (max-width: 768px) {
-    width: calc(100vw - 20px); /* 모바일 화면 */
-  }
+    width: calc(100vw - 20px);
 
   @media (max-width: 480px) {
-    width: calc(100vw - 10px); /* 폰 화면 */
+    width: calc(100vw - 10px);
     margin: 10px 5px 0 5px;
   }
-`;
+`; */
 
-interface EpisodeData {
+/* interface EpisodeData {
   resourceId: number;
   collectionId: number;
   collectionTitle: string;
@@ -129,12 +126,12 @@ interface EpisodeData {
 interface TabProps {
   recentlyWatched: EpisodeData[];
   completedCollections: CompletedCollectionData[];
-}
+} */
 
-const Tab: React.FC<TabProps> = ({ recentlyWatched, completedCollections }) => {
+const TabHeader: React.FC = () => {
   const router = useRouter();
-  const { tab } = router.query; // 쿼리에서 tab 값 가져오기
-  const [isSelected, setIsSelected] = useState(true);
+  // const { tab } = router.query; // 쿼리에서 tab 값 가져오기
+  const isLearned = router.pathname === '/mypage';
 
   // const selectMenuHandler = () => {
   //   if (isSelected == true) {
@@ -144,35 +141,34 @@ const Tab: React.FC<TabProps> = ({ recentlyWatched, completedCollections }) => {
   //   }
   // };
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (tab === 'liked') {
       setIsSelected(false);
     } else {
       setIsSelected(true);
     }
-  }, [tab]);
-
-  const selectMenuHandler = () => {
-    setIsSelected((prev) => !prev);
-  };
+  }, [tab]); */
 
   return (
     <Container>
       <TabMenu>
         <Menu>
-          <MenuName selected={isSelected} onClick={selectMenuHandler}>
+          <MenuName learned={isLearned} onClick={() => router.push('/mypage')}>
             나의 학습방
           </MenuName>
-          <Line width={'99px'} selected={isSelected} />
+          <Line width={'99px'} learned={isLearned} />
         </Menu>
         <Menu>
-          <MenuName selected={!isSelected} onClick={selectMenuHandler}>
+          <MenuName
+            learned={!isLearned}
+            onClick={() => router.push('/mypage/Liked')}
+          >
             관심 컬렉션
           </MenuName>
-          <Line width={'99px'} selected={!isSelected} />
+          <Line width={'99px'} learned={!isLearned} />
         </Menu>
       </TabMenu>
-      <TabContents>
+      {/* <TabContents>
         <TabContent shown={isSelected}>
           <NotCompleted episodes={recentlyWatched} />
           <Completed completedCollections={completedCollections} />
@@ -180,9 +176,9 @@ const Tab: React.FC<TabProps> = ({ recentlyWatched, completedCollections }) => {
         <TabContent shown={!isSelected}>
           <Liked />
         </TabContent>
-      </TabContents>
+      </TabContents> */}
     </Container>
   );
 };
 
-export default Tab;
+export default TabHeader;
--- src/pages/mypage/Liked.tsx
+++ src/pages/mypage/Liked.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
+import styled from 'styled-components';
 import Header from '@/components/header';
 import ProfileBanner from '@/components/mypage/profileBanner';
 import TabHeader from '../../components/mypage/tabHeader';
-import NotCompleted from '../../components/mypage/notCompleted';
-import Completed from '../../components/mypage/completed';
+import Liked from '../../components/mypage/liked';
 import Footer from '@/components/homeFooter';
 import { CompletedCollectionData } from '@/types/types';
 
@@ -27,51 +26,6 @@ const PageContainer = styled.div`
   @media (max-width: 768px) {
     width: calc(100vw - 20px);
   }
-
-  @media (max-width: 480px) {
-    width: calc(100vw - 10px);
-    margin: 10px 5px 0 5px;
-  }
-`;
-
-const LearnedWrapper = styled.div`
-  display: flex;
-  width: 1200px;
-  flex-direction: column;
-  align-items: flex-start;
-  display: flex;
-  gap: 54px;
-
-  @media (max-width: 1024px) {
-    width: 1000px;
-    margin: 20px 10px 0 10px;
-  }
-
-  @media (max-width: 768px) {
-    width: calc(100vw - 20px);
-
-  @media (max-width: 480px) {
-    width: calc(100vw - 10px);
-    margin: 10px 5px 0 5px;
-  }
-`;
-
-const CompletedWrapper = styled.div`
-  display: flex;
-  width: 1200px;
-  flex-direction: column;
-  align-items: flex-start;
-  display: flex;
-  gap: 54px;
-  margin-top: 54px;
-
-  @media (max-width: 1024px) {
-    width: 1000px;
-    margin: 20px 10px 0 10px;
-  }
-
-  @media (max-width: 768px) {
-    width: calc(100vw - 20px);
 
   @media (max-width: 480px) {
     width: calc(100vw - 10px);
@@ -170,12 +124,7 @@ const MyPage = () => {
       <ProfileBanner userInfo={userInfo} />
       <TabHeader />
       <PageContainer>
-        <LearnedWrapper>
-          <NotCompleted episodes={recentlyWatched} />
-        </LearnedWrapper>
-        <CompletedWrapper>
-          <Completed completedCollections={completedCollections} />
-        </CompletedWrapper>
+      <Liked />
       </PageContainer>
       <Footer />
     </LoginProtected>
