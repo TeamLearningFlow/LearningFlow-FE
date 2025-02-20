2978d5ee2c4ea7ac2db923d7cabcaa885011f859
Jinx921 <hyejin020921@sookmyung.ac.kr>
2025-02-20 16:37:59 +0900

    fix: 이메일 변경 토큰 저장 로직 수정 #65

--- src/pages/user/change-email.tsx
+++ src/pages/user/change-email.tsx
@@ -6,6 +6,30 @@ const VerifyEmail: React.FC = () => {
   const router = useRouter();
   const { emailResetCode } = router.query; // URL에서 token 추출
 
+// 유저 정보 갱신(변경 이메일 적용)
+  const fetchUserData = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        console.error('로그인이 필요한 서비스입니다.');
+        return;
+      }
+
+      const response = await axios.get('https://onboarding.p-e.kr/user', {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (response.data.isSuccess) {
+        console.log('유저 정보 갱신 성공:', response.data);
+        localStorage.setItem('email', response.data.result.email); // 최신 이메일 반영
+      } else {
+        console.error('유저 정보 갱신 실패');
+      }
+    } catch (error) {
+      console.error('유저 정보 가져오기 실패:', error);
+    }
+  };
+
   useEffect(() => {
     const validateToken = async () => {
       if (!emailResetCode) return; // 토큰이 없는 경우 작업 중단
@@ -35,7 +59,9 @@ const VerifyEmail: React.FC = () => {
           // 로컬 스토리지에 토큰 저장
           localStorage.setItem('emailResetCode', emailResetCode as string);
           console.log('토큰 저장 완료');
-          
+
+          await fetchUserData(); // 갱신된 이메일 반영한 유저 정보 가져오기
+        
           router.replace(`/mypage/profile?emailResetCode=${emailResetCode}`);
         } else {
           console.log('토큰 무효');
