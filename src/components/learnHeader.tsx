a1230e8dbb434512e097b1236859f083825e2079
장자윤 <jayoonchang@naver.com>
2025-02-21 11:01:22 +0900

    fix: 수강페이지 헤더 프로필 이미지

--- src/components/learnHeader.tsx
+++ src/components/learnHeader.tsx
@@ -1,10 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
 import Link from 'next/link';
 import styled from 'styled-components';
 import Image from 'next/image';
 import LogoDark from '/public/logo_dark.png';
 import Guest from '/public/Guest.svg';
 import UserModal from './modal/userModal';
+import { useRouter } from 'next/router';
 
 const HeaderWrapper = styled.header`
   display: flex;
@@ -51,6 +53,7 @@ const ProfileIcon = styled.div`
 `;
 
 const ProfileImage = styled(Image)`
+  border-radius: 50%;
   @media (max-width: 850px) {
     width: 35px;
     height: 35px;
@@ -72,10 +75,94 @@ const ProfileUser = styled.div`
   }
 `;
 
+interface UserData {
+  name: string;
+  email: string;
+  job: string;
+  interestFields: string[];
+  preferType: string;
+  profileImgUrl: string;
+  bannerImgUrl: string;
+}
+
 const Header: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
   const profileIconRef = useRef<HTMLImageElement | null>(null);
+  const [socialType, setSocialType] = useState<string | null>(null); // 소셜 타입 가져오기
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [userProfile, setUserProfile] = useState<string | null>(null);
+  const router = useRouter();
+
+  const getValidToken = () => {
+    const token = localStorage.getItem('token');
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (token) return token;
+    if (refreshToken) {
+      console.log('기존 토큰 만료, 리프레시 토큰 사용');
+      return refreshToken;
+    }
+
+    console.error('토큰 없음, 재로그인 필요');
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    router.replace('/login');
+    return null;
+  };
+  const fetchUserData = async () => {
+    const validToken = getValidToken();
+    if (!validToken) return;
+
+    try {
+      // 로컬 스토리지에서 토큰 가져오기 (로그인 시에만 접근 가능)
+      // const token = localStorage.getItem('token');
+      // const refreshToken = localStorage.getItem('refreshToken');
+      // console.log('현재 토큰:', token);
+      const storedSocialType = localStorage.getItem('socialType');
+      setSocialType(storedSocialType);
+      // console.log('소셜 타입:', storedSocialType)
+
+      // if (!token) {
+      // console.error('로그인이 필요한 서비스입니다.');
+      // return;
+      // }
+
+      // Authorization 헤더 추가
+      const response = await axios.get('https://onboarding.p-e.kr/user', {
+        headers: {
+          Authorization: `Bearer ${validToken}`,
+          // 'Refresh-Token': `Bearer ${refreshToken}`,
+        },
+      });
+
+      console.log('Profile Response Data:', response.data);
+      setUserData(response.data.result);
+      setUserProfile(response.data.result.imageUrl);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 401) {
+          console.error(
+            '토큰이 만료되었거나 잘못되었습니다. 다시 로그인해주세요.',
+          );
+          localStorage.removeItem('token'); // 만료된 토큰 삭제
+        } else {
+          console.error(
+            '사용자 정보 가져오기 실패:',
+            error.response?.data || error.message,
+          );
+        }
+      } else if (error instanceof Error) {
+        console.error(error.message);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
 
   // 사용자 아이콘 클릭 시 모달 토글
   const toggleModal = () => {
@@ -106,6 +193,10 @@ const Header: React.FC = () => {
     };
   }, [isModalOpen]);
 
+  if (!userData) {
+    return <p>정보 없음</p>;
+  }
+
   return (
     <HeaderWrapper>
       <LogoWrapper>
@@ -114,14 +205,25 @@ const Header: React.FC = () => {
         </Link>
       </LogoWrapper>
       <ProfileIcon>
-        <ProfileImage
-          src={Guest}
-          alt="profile"
-          width={42}
-          height={42}
-          onClick={toggleModal}
-          ref={profileIconRef}
-        />
+        {userData.profileImgUrl ? (
+          <ProfileImage
+            src={userData.profileImgUrl}
+            alt="profile"
+            width={42}
+            height={42}
+            onClick={toggleModal}
+            ref={profileIconRef}
+          />
+        ) : (
+          <ProfileImage
+            src={Guest}
+            alt="profile"
+            width={42}
+            height={42}
+            onClick={toggleModal}
+            ref={profileIconRef}
+          />
+        )}
       </ProfileIcon>
       {isModalOpen && (
         <ProfileUser ref={modalRef}>
